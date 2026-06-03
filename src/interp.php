d90985faa9375a081d38eb1d0e68750c19d3c998
Tan Nguyen <nguyennhattan2707@gmail.com>
2026-06-03 15:46:47 -0700

    Update interp.php as person 3

--- src/interp.php
+++ src/interp.php
@@ -66,13 +66,25 @@ function interpIf(array $expr, array $env): array
 
 function interpFn(array $expr, array $env): array
 {
+    return closureV($expr['params'], $expr['body'], $env);
     // Person 3: create a closureV that stores params, body, and the saved env.
-    vebgError('function expressions are assigned to Person 3');
+}
+
+function applyClosure(array $closure, array $args): array
+{
+    $closure['env'] = extendMany($closure['env'], $closure['params'], $args);
+    return interp($closure['body'], $closure['env']);
 }
 
 function interpApp(array $expr, array $env): array
 {
+    $fn = interp($expr['fn'], $env);
+    $evalArgs = array_map(fn($arg) => interp($arg, $env), $expr['args']);
+    return match ($fn['tag'] ?? null) {
+        'primV' => applyPrimop($fn['name'], $evalArgs),
+        'closureV' => applyClosure($fn, $evalArgs),
+        default => vebgError('expected function value in application'),
+    };
     // Person 3: evaluate the function position and arguments, then dispatch to
     // closure application or Person 2's applyPrimop for primitive values.
-    vebgError('applications are assigned to Person 3');
 }
